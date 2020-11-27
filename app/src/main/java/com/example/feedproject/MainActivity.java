612e5d2e669ce3c7ec6f9984c9f53ed8677a388a
jhuinee <jhuinee12@gmail.com>
2020-11-27 21:39:15 +0900

    DogMainActivity.java 연결

--- app/src/main/java/com/example/feedproject/MainActivity.java
+++ app/src/main/java/com/example/feedproject/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.feedproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bt_dog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt_dog = (Button)findViewById(R.id.btnDog);
+
+        bt_dog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), DogMainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
